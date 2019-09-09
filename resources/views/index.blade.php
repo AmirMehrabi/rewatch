41c798f3b08e6be6a13e4e221c27dcb6585e1e9d
Amir Mehrabi <aut0run2011@gmail.com>
2019-09-10 03:11:43 +0430

    Created a login page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -68,8 +68,6 @@
         </div>
     </nav>
     <div class="container mx-auto px-10 md:px-4">
-
-
         <div class="px-2">
             <div class="flex flex-wrap -mx-2">
                 <!-- <div class="w-full md:w-1/2  lg:w-1/3 xl:w-1/4 md:mb-5 px-2 mb-4 ">
@@ -203,6 +201,5 @@
                 <!-- Birthday boy! -->
             </div>
         </div>
-    </div>
     </div>
 @endsection
