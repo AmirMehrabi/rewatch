1745e95522fe0d0ef9836dad4a1d58200b0b9b20
Amir Mehrabi <aut0run2011@gmail.com>
2019-09-10 03:08:48 +0430

    Created the Index page, nice and clean!

---
+++ resources/views/index.blade.php
@@ -0,0 +1,208 @@
+@extends('layouts.main')
+
+
+@section('content')
+
+<section class="bg-white">
+    <div class="container mx-2 md:mx-0">
+                <div class="hidden lg:flex lg:flex-row w-full md:w-4/5 lg:w-2/3 xl:w-2/4 mx-auto items-center content-end  py-8">
+                    <form class="w-full mb-8">
+                        <div class="text-center">
+                            <p class="text-base sm:text-lg md:text-xl xl:text-2xl py-8 text-gray-700 font-semibold">
+                                دیگر تولد دوستان و آشنایان خود را فراموش نکنید.
+                                <span class="text-xs sm:text-sm md:text-base font-light">روز قبل شما را مطلع خواهیم کرد.</span>
+                            </p>
+                        </div>
+                        <div
+                            class="flex shadow-xl py-1 sm:py-3 md:py-4 items-center bg-white rounded-lg appearance-none border border-gray-200 mb-6 rounded w-full px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                            <input
+                                class="text-xs sm:text-sm md:text-base appearance-none bg-transparent border-none w-full text-gray-700 mr-3 irsans py-1 px-2 leading-tight focus:outline-none"
+                                type="text" placeholder="امیر مهرابیان ..." aria-label="Full name">
+                            <input
+                                class="text-xs sm:text-sm md:text-base appearance-none bg-transparent border-none w-full text-gray-700 mr-3 irsans py-1 px-2 leading-tight focus:outline-none"
+                                type="text" placeholder="۱۸/۰۶/۱۳۷۴" aria-label="Full name">
+                            <button
+                                class="text-xs sm:text-sm flex-shrink-0 irsans py-2 px-4 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 border-4 text-white py-1 px-2 rounded"
+                                type="button">
+                                ذخیره
+                            </button>
+                            <!-- <button
+                                                class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+                                                type="button">
+                                                Cancel
+                                            </button> -->
+                        </div>
+                    </form>
+                </div>
+    </div>
+</section>
+
+    <nav class="flex items-center justify-between flex-wrap bg-gray-100 p-5 irsans mb-8 ">
+        <div class="w-full  flex-grow flex items-center w-auto">
+            <div class="text-sm flex-grow">
+                <a href="#responsive-header"
+                    class=" inline-block text-gray-900 bg-gray-100 px-1 sm:px-2 md:px-3 lg:px-4 py-1 md:py-2 rounded rounded-lg hover:text-blue-500 hover:bg-blue-100">
+                    دوستان
+                </a>
+                <a href="#responsive-header"
+                    class=" inline-block text-gray-900 bg-gray-100 px-1 sm:px-2 md:px-3 lg:px-4 py-1 md:py-2 rounded rounded-lg hover:text-blue-500 hover:bg-blue-100">
+                    خانواده
+                </a>
+                <a href="#responsive-header"
+                    class=" inline-block text-gray-900 bg-gray-100 px-1 sm:px-2 md:px-3 lg:px-4 py-1 md:py-2 rounded rounded-lg hover:text-blue-500 hover:bg-blue-100">
+                    همکاران
+                </a>
+                <a href="#responsive-header"
+                    class=" inline-block text-gray-900 bg-gray-100 px-1 sm:px-2 md:px-3 lg:px-4 py-1 md:py-2 rounded rounded-lg hover:text-blue-500 hover:bg-blue-100">
+                    هم‌کلاسی‌ها
+                </a>
+                <a href="#responsive-header"
+                    class=" inline-block text-gray-900 bg-gray-100 px-1 sm:px-2 md:px-3 lg:px-4 py-1 md:py-2 rounded rounded-lg hover:text-blue-500 hover:bg-blue-100">
+                    متفرقه
+                </a>
+            </div>
+            <div>
+                <a href="#"
+                    class="inline-block text-sm px-4 py-2 leading-none  rounded  text-blue-500 hover:bg-blue border-blue-400 hover:bg-blue-100 hover:border-transparent hover:text-gray hover:text-blue-400 hover:bg-blue-100">فیلتر</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container mx-auto px-10 md:px-4">
+
+
+        <div class="px-2">
+            <div class="flex flex-wrap -mx-2">
+                <!-- <div class="w-full md:w-1/2  lg:w-1/3 xl:w-1/4 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg hover:shadow-2xl city-card">
+                        <img src="images/kerman.jpg"
+                            class="w-full h:64 md:h-56  object-cover city-image rounded-t rounded-t-lg" alt="">
+                        <div class=" flex flex-row text-2xl irsans text-black items-center p-4">
+                            <div class="flex-1 text-right">
+                                کرمان
+                            </div>
+                            <div class="flex-1 text-left">
+                                <a href="#"
+                                    class="inline-block text-sm px-4 py-2 leading-none border rounded rounded-lg text-blue-600 border-blue-500 irsans hover:border-transparent hover:bg-teal-100 hover:text-teal-600 hover:bg-white mt-4 lg:mt-0 btn-city">مشاهده‌ی
+                                    شهر</a>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+                <!-- Birthday Card -->
+                <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg  p-1 hover:shadow-2xl city-card flex flex-col">
+                        <div class="text-gray-700 text-center  p-2 text-right">
+                            <a href="#" class="text-gray-500 hover:text-indigo-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                        </div>
+                        <div class="text-gray-700 text-center  px-4">
+                            <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                            <p class="text-sm md:text-base md:text-lg font-medium text-black p-2 pb-1">امیر مهرابیان</p>
+                            <p class="text-xs sm:text-sm md:text-base pb-2">هجدهم شهریور ۱۳۷۴</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Birthday Card -->
+
+                <!-- Birthday Card -->
+                <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg  p-1 hover:shadow-2xl city-card flex flex-col">
+                        <div class="text-gray-700 text-center  p-2 text-right">
+                            <a href="#" class="text-gray-500 hover:text-indigo-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                        </div>
+                        <div class="text-gray-700 text-center  px-4">
+                            <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                            <p class="text-sm md:text-base md:text-lg font-medium text-black p-2 pb-1">امیر مهرابیان</p>
+                            <p class="text-xs sm:text-sm md:text-base pb-2">هجدهم شهریور ۱۳۷۴</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Birthday Card -->
+
+                <!-- Birthday Card -->
+                <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg  p-1 hover:shadow-2xl city-card flex flex-col">
+                        <div class="text-gray-700 text-center  p-2 text-right">
+                            <a href="#" class="text-gray-500 hover:text-indigo-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                        </div>
+                        <div class="text-gray-700 text-center  px-4">
+                            <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                            <p class="text-sm md:text-base md:text-lg font-medium text-black p-2 pb-1">امیر مهرابیان</p>
+                            <p class="text-xs sm:text-sm md:text-base pb-2">هجدهم شهریور ۱۳۷۴</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Birthday Card -->
+                
+                <!-- Birthday Card -->
+                <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg  p-1 hover:shadow-2xl city-card flex flex-col">
+                        <div class="text-gray-700 text-center  p-2 text-right">
+                            <a href="#" class="text-gray-500 hover:text-indigo-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                        </div>
+                        <div class="text-gray-700 text-center  px-4">
+                            <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                            <p class="text-sm md:text-base md:text-lg font-medium text-black p-2 pb-1">امیر مهرابیان</p>
+                            <p class="text-xs sm:text-sm md:text-base pb-2">هجدهم شهریور ۱۳۷۴</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Birthday Card -->
+                <!-- Birthday Card -->
+                <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg  p-1 hover:shadow-2xl city-card flex flex-col">
+                        <div class="text-gray-700 text-center  p-2 text-right">
+                            <a href="#" class="text-gray-500 hover:text-indigo-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                        </div>
+                        <div class="text-gray-700 text-center  px-4">
+                            <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                            <p class="text-sm md:text-base md:text-lg font-medium text-black p-2 pb-1">امیر مهرابیان</p>
+                            <p class="text-xs sm:text-sm md:text-base pb-2">هجدهم شهریور ۱۳۷۴</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Birthday Card -->
+                
+                <!-- Birthday Card -->
+                <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 md:mb-5 px-2 mb-4 ">
+                    <div class="bg-white  rounded-lg  p-1 hover:shadow-2xl city-card flex flex-col">
+                        <div class="text-gray-700 text-center  p-2 text-right">
+                            <a href="#" class="text-gray-500 hover:text-indigo-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                        </div>
+                        <div class="text-gray-700 text-center  px-4">
+                            <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                            <p class="text-sm md:text-base md:text-lg font-medium text-black p-2 pb-1">امیر مهرابیان</p>
+                            <p class="text-xs sm:text-sm md:text-base pb-2">هجدهم شهریور ۱۳۷۴</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of Birthday Card -->
+
+
+                <!-- Birthday Boy! -->
+                <div class="w-4/5 md:w-3/5 lg:w-2/5 mx-auto px-2 mb-4 ">
+                    <div class="bg-indigo-400 rounded-lg p-1 hover:shadow-2xl city-card flex flex-col">
+
+                        <div class="text-white">
+                            <div class="flex flex-row  items-center justify-center">
+                                <div class="text-white m-2 w-2/5 xl:w-auto">
+                                    <!-- <div class="text-right">
+                                        <a href="#" class="text-white text-right hover:text-yellow-500 rounded-full"><i class="fas fa-ellipsis-h"></i></a>
+                                    </div> -->
+                                                                
+
+                                    <img src="images/avatars/1.jpg" class="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full  flex items-center justify-center mx-auto" alt="">
+                                    <p class="text-base pt-2">امیر مهرابیان</p>
+                                </div>
+                                <div class="text-white text-center m-2 w-3/5  xl:w-auto">
+                                    <h1 class="text-base sm:text-lg md:text-xl font-light text-white lg:text-right">تولد امیر امروز در راه است. <span class="font-semibold">تبریک</span> فراموش نشود!</h1>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
+                </div>
+                <!-- Birthday boy! -->
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>کوک | @yield('title')</title>
+    <meta name="author" content="name" />
+    <meta name="description" content="description here" />
+    <meta name="keywords" content="keywords,here" />
+    <link href="{{asset('fontawesome/css/all.css')}}" rel="stylesheet" />
+    <link href=" {{asset('css/app.css')}}" rel="stylesheet" />
+    <!--Replace with your tailwind.css once created-->
+    <link href="{{asset('css/animate.min.css')}}" rel="stylesheet" />
+</head>
+
+<body class="font-sans leading-normal tracking-normal">
+    <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-3 irsans">
+        <div class="flex items-center flex-shrink-0 text-white mx-6">
+            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+            </svg>
+            <span class="font-semibold text-xl tracking-tight mr-2">یادکرد</span>
+        </div>
+        <div class="block md:hidden">
+            <button
+                class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+            </button>
+        </div>
+        <div class="w-full block flex-grow md:flex md:items-center md:w-auto">
+            <div class="text-sm md:flex-grow">
+                <!-- <a href="#responsive-header"
+                    class="block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white mr-4">
+                    بهترین 
+                </a>
+                <a href="#responsive-header"
+                    class="block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white mr-4">
+                    لیست شهرها
+                </a>
+                <a href="#responsive-header"
+                    class="block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white mr-4">
+                    فودی‌ها
+                </a>
+                <a href="#responsive-header"
+                    class="block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white mr-4">
+                    پیشنهاد شهر
+                </a> -->
+            </div>
+            <div>
+                <a href="#"
+                    class="hidden md:inline-block text-sm px-4 py-2 leading-none border rounded rounded-lg text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white">ورود</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Second nav -->
+
+    @yield('content')
+
+
+
+
+
+    </div>
+
+    <script src="https://unpkg.com/popper.js@1/dist/umd/popper.min.js"></script>
+    <script src="https://unpkg.com/tippy.js@4"></script>
+    <script>
+        //Init tooltips
+        tippy(".avatar");
+    </script>
+</body>
+
+</html>
