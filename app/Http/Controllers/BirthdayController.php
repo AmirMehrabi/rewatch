0166d48a461925a4d00e8d24e499432c5cfd6a75
Amir Mehrabi <aut0run2011@gmail.com>
2019-09-10 20:55:05 +0430

    Created a controller, model and migration

---
+++ app/Http/Controllers/BirthdayController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Birthday;
+use Illuminate\Http\Request;
+
+class BirthdayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Birthday  $birthday
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Birthday $birthday)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Birthday  $birthday
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Birthday $birthday)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Birthday  $birthday
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Birthday $birthday)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Birthday  $birthday
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Birthday $birthday)
+    {
+        //
+    }
+}
