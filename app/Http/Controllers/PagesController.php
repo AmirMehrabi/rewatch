7d0c4d6e28c0ab2490f3c69c264ca3ebaed742b8
Amir Mehrabi <aut0run2011@gmail.com>
2019-10-31 16:13:23 +0330

    Ctrated a controller to send notifications

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -18,7 +18,20 @@ class PagesController extends Controller
         $tomorrow_birthdays = Birthday::whereMonth('birthday_date', '=', date('m'))->whereDay('birthday_date', '=', date('d') +1)->get();
         
         foreach ($tomorrow_birthdays as $birthday) {
-            echo $birthday->user->phone;
+            // echo $birthday->user->phone;
+            try {
+                $sender = "10004346";
+                $message = "فردا تولد {$birthday->name} است. فراموشتان نشود.";
+                $receptor = array($birthday->user->phone);
+                $result = Kavenegar::Send($sender, $receptor, $message);
+            } catch (\Kavenegar\Exceptions\ApiException $e) {
+                // در صورتی که خروجی وب سرویس 200 نباشد این خطا رخ می دهد
+                // echo $e->errorMessage();
+                return Redirect::back()->withErrors(['error', $e->errorMessage()]);
+            } catch (\Kavenegar\Exceptions\HttpException $e) {
+                // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
+                return Redirect::back()->withErrors(['error', $e->errorMessage()]);
+            }
         }
         
     }
