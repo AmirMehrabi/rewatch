546e1c4958952bcdd03e51f82af2f2bd974b21bb
Amir Mehrabi <aut0run2011@gmail.com>
2019-11-05 23:29:20 +0330

    Created a workbox file

---
+++ workbox.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "globDirectory": "public/",
+    "globPatterns": [
+      "**/*.{png,xml,css,ico,jpg,svg,js,json}"
+    ],
+    "swDest": "public\\service-worker.js",
+    "swSrc": "resources/js/service-worker.js"
+  };
